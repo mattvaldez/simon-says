c46b2eadf80cddfb331b77339eeaae72584a30bd
Matt <matvaldez@gmail.com>
2015-05-13 17:33:13 -0400

    working on building arrays

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,30 @@
 $(function(){	
        var pattern = [];
-       var choice = Math.random(); 
-       var green = $(function(){$('div.button-green').addClass('green-light').removeClass('green-light');});
-       var yellow = $(function(){$('div.button-yellow').addClass('yellow-light').removeClass('yellow-light');});
-       var red = $(function(){$('div.button-red').addClass('red-light').removeClass('red-light');});
-       var blue = $(function(){$('div.button-blue').addClass('blue-light').removeClass('blue-light');});
-
+       var player = [];
+       
+       //var green = $(function(){$('div.button-green').addClass('green-light').removeClass('green-light');});
+       //var yellow = $(function(){$('div.button-yellow').addClass('yellow-light').removeClass('yellow-light');});
+       //var red = $(function(){$('div.button-red').addClass('red-light').removeClass('red-light');});
+       //var blue = $(function(){$('div.button-blue').addClass('blue-light').removeClass('blue-light');});
+     
   
 
-//----------------------------------------------------------computer pattern
+//----------------------------------------------------------computer turn
        
-   function computerTurn(){
-       
-    
-           
+   function computerChoose(){
+            
+       var choice = Math.random(); 
        if(choice < 0.25 ){
-           pattern.push(green);
+           pattern.push('green');
        }
        else if(choice < 0.5 ){
-           pattern.push(yellow);      
+           pattern.push('yellow');      
        }
        else if(choice < 0.75 ){
-           pattern.push(red);    
+           pattern.push('red');    
        }
        else{
-           pattern.push(blue);    
+           pattern.push('blue');    
        }
        return pattern;
    }
@@ -35,28 +35,71 @@ $(function(){
   
 
 
-//----------------------------------------------------------player input	
-	$('div.button-green').mousedown(function(){
-		$(this).addClass('green-light').mouseup(function(){
+//----------------------------------------------------------player turn	
+	
+
+   $('div.button-green').mousedown(function(){
+      $(this).addClass('green-light').mouseup(function(){
           $(this).removeClass('green-light');
-		});
-    });
-
-$('div.button-red').mousedown(function(){
-		$(this).addClass('red-light').mouseup(function(){
+          player.push('green');
+          console.log(player);
+      });
+      });
+          
+   
+  
+  $('div.button-red').mousedown(function(){
+  		$(this).addClass('red-light').mouseup(function(){
           $(this).removeClass('red-light');
-		});
-    });
+          player.push('red');
+          console.log(player);
+      });
+      });
+          
+   
+  
+  $('div.button-yellow').mousedown(function(){
+  		$(this).addClass('yellow-light').mouseup(function(){
+            $(this).removeClass('yellow-light');
+            player.push('yellow');
+            console.log(player);
+      });
+      });
+          
+   
+  $('div.button-blue').mousedown(function(){
+  		$(this).addClass('blue-light').mouseup(function(){
+            $(this).removeClass('blue-light');
+            player.push('blue');
+            console.log(player);
+      });
+      });
+          
+ function playAgain(){
+  var match = (player.length == pattern.length) && player.every(function(element, index){
+    return element === pattern[index]
+  });
+  if(match = true){
+    computerChoose();
+  }
+  
+  else{
+    alert('try again');
+  }
+}  
+      
+computerChoose();
+console.log(pattern);
+playAgain();
 
-$('div.button-yellow').mousedown(function(){
-		$(this).addClass('yellow-light').mouseup(function(){
-          $(this).removeClass('yellow-light');
-		});
-    });
 
-$('div.button-blue').mousedown(function(){
-		$(this).addClass('blue-light').mouseup(function(){
-          $(this).removeClass('blue-light');
-		});
-    });
+
 });
+
+
+  
+
+  
+
+
+
