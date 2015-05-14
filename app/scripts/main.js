d6a7ffecd0a81a1bc4c49286823e09a4732b64e8
Matt <matvaldez@gmail.com>
2015-05-14 00:46:40 -0400

    got small parts of the js program to work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,7 @@
 $(function(){	
        var pattern = [];
-       var player = [];
-       
-       //var green = $(function(){$('div.button-green').addClass('green-light').removeClass('green-light');});
-       //var yellow = $(function(){$('div.button-yellow').addClass('yellow-light').removeClass('yellow-light');});
-       //var red = $(function(){$('div.button-red').addClass('red-light').removeClass('red-light');});
-       //var blue = $(function(){$('div.button-blue').addClass('blue-light').removeClass('blue-light');});
-     
-  
-
-//----------------------------------------------------------computer turn
-       
+       var player = [];              
+//----------------------------------------------------------computer turn     
    function computerChoose(){
             
        var choice = Math.random(); 
@@ -27,79 +18,61 @@ $(function(){
            pattern.push('blue');    
        }
        return pattern;
-   }
-
-   
-
-
-  
-
-
-//----------------------------------------------------------player turn	
-	
-
+   } 
+//----------------------------------------------------------player button lights	
    $('div.button-green').mousedown(function(){
       $(this).addClass('green-light').mouseup(function(){
-          $(this).removeClass('green-light');
-          player.push('green');
-          console.log(player);
-      });
-      });
-          
-   
-  
+          $(this).removeClass('green-light');    
+      });}); 
   $('div.button-red').mousedown(function(){
   		$(this).addClass('red-light').mouseup(function(){
           $(this).removeClass('red-light');
-          player.push('red');
-          console.log(player);
-      });
-      });
-          
-   
-  
+      });});
   $('div.button-yellow').mousedown(function(){
   		$(this).addClass('yellow-light').mouseup(function(){
-            $(this).removeClass('yellow-light');
-            player.push('yellow');
-            console.log(player);
-      });
-      });
-          
-   
+            $(this).removeClass('yellow-light');      
+      });});
   $('div.button-blue').mousedown(function(){
   		$(this).addClass('blue-light').mouseup(function(){
-            $(this).removeClass('blue-light');
-            player.push('blue');
-            console.log(player);
-      });
-      });
-          
+            $(this).removeClass('blue-light');    
+      });});     
+//----------------------------------------------------------player move input
+   $('div.button-green').click(function(){
+           player.push('green');
+           console.log(player);
+           return player;
+   });
+   $('div.button-yellow').click(function(){
+           player.push('yellow');
+           return player;
+   });
+   $('div.button-red').click(function(){
+           player.push('red');
+           return player;
+   });
+   $('div.button-blue').click(function(){
+           player.push('blue');
+   });      return player; 
+//----------------------------------------------------------start the game
+ function startGame(){
+   $('div.start').click(function(){
+        computerChoose();
+        console.log(pattern);
+   }); 
+
+ }
+
+
+//-----------------------------------------------------------compare arrays
  function playAgain(){
-  var match = (player.length == pattern.length) && player.every(function(element, index){
-    return element === pattern[index]
-  });
-  if(match = true){
+  var man = _.last(player);
+  var machine = _.last(pattern);
+  if(man === machine){
     computerChoose();
   }
-  
   else{
     alert('try again');
   }
-}  
-      
-computerChoose();
-console.log(pattern);
-playAgain();
-
-
+ } 
 
 });
-
-
-  
-
-  
-
-
-
