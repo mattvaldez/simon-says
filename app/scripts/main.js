a0042b843b7ea7a6e74a8eb15c988933f1eb41fa
Matt <matvaldez@gmail.com>
2015-05-15 00:38:29 -0400

    developing gameplay model, working on player input

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,13 @@
-//'use strict';
-
+'use strict';
 $(function(){	
        var pattern = [];
        var player = [];              
 //----------------------------------------------------------start the game
-
+   
 $('button').click(function(){
+        console.log('the game starts here');
         computerChoose();
-        console.log(pattern);
-   })
+   });
 
 //----------------------------------------------------------computer turn     
    function computerChoose(){
@@ -16,97 +15,81 @@ $('button').click(function(){
        var choice = Math.random(); 
        if(choice < 0.25 ){
            pattern.push('green');
+           
        }
        else if(choice < 0.5 ){
-           pattern.push('yellow');      
+           pattern.push('yellow');
+                 
        }
        else if(choice < 0.75 ){
-           pattern.push('red');    
+           pattern.push('red');
+               
        }
        else{
-           pattern.push('blue');    
+           pattern.push('blue');
+               
        }
-       gameFlow();
-       return pattern;
+       console.log(pattern);
+       playerMove();
+           
    }
+//----------------------------------------------------------player move input
+   function playerMove(){
+    if( color = green){
+      player.push('green');
+      console.log(player);
+    }
+    else if( color = red){
+      player.push('red');
+      console.log(player);
+    }
+    else if( color = yellow){
+      player.push('yellow');
+      console.log(player);
+    }
+    else if( color = blue){
+      player.push('blue');
+      console.log(player);
+    }
+    checkPlayer();
 
-//----------------------------------------------------------player button lights	
+
+   } 
+//----------------------------------------------------------player array builder
+   function checkPlayer(){
+    var i = 0;
+    var x = pattern[i].length;
+    var y = player[0].length;
+      if (x === y){
+        console.log('move matched, continue');
+        player=[];
+      }
+      else{
+        console.log('sorry, try again');
+        player = [];
+        pattern = [];
+      }
+    computerChoose();
+    console.log(pattern +' pattern');
+  }
+
+   
+//----------------------------------------------------------player button lights  
    $('div.button-green').mousedown(function(){
       $(this).addClass('green-light').mouseup(function(){
           $(this).removeClass('green-light');    
       });}); 
   $('div.button-red').mousedown(function(){
-  		$(this).addClass('red-light').mouseup(function(){
+      $(this).addClass('red-light').mouseup(function(){
           $(this).removeClass('red-light');
       });});
   $('div.button-yellow').mousedown(function(){
-  		$(this).addClass('yellow-light').mouseup(function(){
+      $(this).addClass('yellow-light').mouseup(function(){
             $(this).removeClass('yellow-light');      
       });});
   $('div.button-blue').mousedown(function(){
-  		$(this).addClass('blue-light').mouseup(function(){
+      $(this).addClass('blue-light').mouseup(function(){
             $(this).removeClass('blue-light');    
       });});     
-//----------------------------------------------------------player move input
-   function playerMove(){
-   
-   $('div.button-green').click(function(){
-    player.push('green')
-    console.log(player);
-    gameFlowEnd();
-   });
-   $('div.button-yellow').click(function(){
-    player.push('yellow')
-    console.log(player);
-    gameFlowEnd();
-   });
-   $('div.button-red').click(function(){
-    player.push('red')
-    console.log(player);
-    gameFlowEnd();
-   });
-   $('div.button-blue').click(function(){
-    player.push('blue')
-    console.log(player);
-    gameFlowEnd();
-   });
-
-   return player;
-    }
- 
-    
-   
-
-//-----------------------------------------------------------real time array comparison
-  function gameFlow(){
-    console.log('the game starts here');
-    playerMove();
-
-}
-//-----------------------------------------------------------ultimate array comparison
-function gameFlowEnd(){
-  
-    var man = player.length;
-    var machine = pattern.length;
-     
-    if(man === machine){
-      
-      console.log(player +' player array for this round');
-      
-      console.log(man, machine, + ' array lengths just compared');
-      player =[];    
-  }  
-    else{
-      console.log('wrong');
-      player =[];
-      pattern = [];
-    }
-  console.log(pattern + ' this is the end of the round');
-  console.log(player + ' players array for new round');
-  computerChoose();
-} 
-
-
-
 
 });
